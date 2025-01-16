f328f0a09f7af7ddec44bc7c7ad61947daab9d46
NaufalFaisal707 <naufalfaisal707@gmail.com>
2025-01-16 14:07:47 +0700

    ganti favicon

--- app/root.tsx
+++ app/root.tsx
@@ -35,10 +35,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
       <head>
-        <Meta />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <Links />
+        <Meta />
       </head>
       <body>
         {children}
